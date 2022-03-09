6d3f3c8145a33b3f652d6b5360a525f16af983b4
Miha Zupan <mihazupan.zupan1@gmail.com>
2022-03-09 22:59:56 +0100

    Update client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,23 +1,31 @@
 ﻿using System.Diagnostics;
 using System.Net;
 
-var handler = new SocketsHttpHandler();
-handler.UseCookies = false;
-handler.SslOptions.RemoteCertificateValidationCallback = delegate { return true; };
-
-var client = new HttpClient(handler);
+var allTimings = new Dictionary<string, List<TimeSpan>>();
 
 foreach (var proxyVersion in new[] { HttpVersion.Version11, HttpVersion.Version20 })
 {
     foreach (var backendVersion in new[] { HttpVersion.Version11, HttpVersion.Version20 })
     {
-        for (var bodySize = 1024 * 256; bodySize <= 1024 * 1024 * 16; bodySize *= 2)
+        for (var bodySize = 1024 * 1024; bodySize <= 1024 * 1024 * 32; bodySize *= 2)
         {
+            var timingKey = $"{proxyVersion}-{backendVersion} {bodySize / 1024} kB";
+            Console.WriteLine(timingKey);
+
             var body = new byte[bodySize];
             Random.Shared.NextBytes(body);
 
-            for (int retry = 1; retry <= 2; retry++)
+            var timings = new List<TimeSpan>();
+            allTimings.Add(timingKey, timings);
+
+            for (int retry = 1; retry <= 50; retry++)
             {
+                using var handler = new SocketsHttpHandler();
+                handler.UseCookies = false;
+                handler.SslOptions.RemoteCertificateValidationCallback = delegate { return true; };
+
+                using var client = new HttpClient(handler);
+
                 const string Proxy = "https://10.0.0.4";
 
                 var request = new HttpRequestMessage
@@ -37,11 +45,26 @@ foreach (var proxyVersion in new[] { HttpVersion.Version11, HttpVersion.Version2
 
                 string responseString = await response.Content.ReadAsStringAsync();
 
-                Console.WriteLine($"{retry} - Sent {bodySize / 1024} kB in {start.ElapsedMilliseconds:N2} ms (HTTP {proxyVersion}-{backendVersion})");
+                start.Stop();
+
+                timings.Add(start.Elapsed);
+
+                //Console.WriteLine($"{retry} - Sent {bodySize / 1024} kB in {start.ElapsedMilliseconds:N2} ms (HTTP {proxyVersion}-{backendVersion})");
                 //Console.WriteLine(responseString);
             }
 
-            Console.WriteLine();
+            //Console.WriteLine();
         }
     }
+}
+
+foreach (var (key, timings) in allTimings)
+{
+    timings.Sort();
+
+    var p50 = (int)timings[(int)(timings.Count * 0.50)].TotalMilliseconds;
+    var p70 = (int)timings[(int)(timings.Count * 0.70)].TotalMilliseconds;
+    var p90 = (int)timings[(int)(timings.Count * 0.90)].TotalMilliseconds;
+
+    Console.WriteLine($"{key,20}: P50={p50,3} P70={p70,3} P90={p90,3}");
 }
--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel(options =>
 {
+    options.Limits.MaxRequestBodySize = 1024 * 1024 * 1024 * 5L; // 5 GB
     options.Limits.Http2.InitialStreamWindowSize = 16 * 1024 * 1024;
     options.Limits.Http2.InitialConnectionWindowSize = 16 * 1024 * 1024;
 });
