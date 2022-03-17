a247885456fc9c89a63abd812303eee0e498d39a
Miha Zupan <mihazupan.zupan1@gmail.com>
2022-03-17 17:43:03 +0100

    Simple proxy

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,31 +1,17 @@
-using System.Diagnostics;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.Limits.MaxRequestBodySize = 1024 * 1024 * 1024 * 5L; // 5 GB
-    options.Limits.Http2.InitialStreamWindowSize = 1 * 1024 * 1024;
-    options.Limits.Http2.InitialConnectionWindowSize = 1 * 1024 * 1024;
-});
+//builder.WebHost.ConfigureKestrel(options =>
+//{
+//    options.Limits.MaxRequestBodySize = 1024 * 1024 * 1024 * 5L; // 5 GB
+//    options.Limits.Http2.InitialStreamWindowSize = 1 * 1024 * 1024;
+//    options.Limits.Http2.InitialConnectionWindowSize = 1 * 1024 * 1024;
+//});
 
 var app = builder.Build();
 
-app.Map("/", async (HttpContext context) =>
+app.Map("/", (HttpContext context) =>
 {
-    var start = Stopwatch.StartNew();
-
-    var buffer = new byte[4096];
-    var totalRead = 0;
-    var read = 0;
-    var body = context.Request.Body;
-    while ((read = await body.ReadAsync(buffer)) > 0) totalRead += read;
-
-    return new ResponseModel(
-        context.Request.Method,
-        context.Request.Path,
-        context.Request.Headers,
-        $"Received {totalRead} bytes in {start.ElapsedMilliseconds:N2} ms");
+    return context.Request.Path;
 });
 
 app.Run();
